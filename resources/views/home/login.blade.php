204bd71a86edf59888a618ead5cf3a00c90d4344
Aleah11 <aleahcordova123@gmail.com>
2025-04-28 16:31:31 +0800

    Edited Login Pafe

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -2,70 +2,92 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Cinema - Login</title>
 
-    <title>Cinema - Login</title>
+  <link href="https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link href="https://fonts.googleapis.com/css2?family=Special+Gothic+Expanded+One&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+  <style>
+    body {
+      font-family: 'Teachers', sans-serif;
+      background-color: #ffffff;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      min-height: 80vh;
+      padding: 1rem;
+    }
 
-    <style>
-        .container1 {
-            margin-top: 200px;
-            width: 600px;
-            height: 330px;
-            border-radius: 50px;
-        }
+    .card {
+      border-radius: 30px;
+      padding: 20px;
+      height: 340px;
+      width: 100%;
+      max-width: 600px;
+      background-color: #f3f3f3;
+    }
 
-        .button {
-            width: 150px;
-            height: 65px;
-            border-radius: 50px;
-            margin: 20px auto 0;
-            display: block;
-        }
+    p {
+      text-align: center;
+      font-size: 75px;
+      margin-bottom: 5px;
+      color: #7e7e7e;
+    }
 
-        body {
-        font-family: 'Special Gothic Expanded One', sans-serif;
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-color: #f8f9fa;
-        }
-    </style>
+    .form-label {
+      color: #7e7e7e;
+      display: block;
+      text-align: center;
+      font-size: 1.8rem;
+      margin-bottom: 0.5rem;
+    }
 
+    .form-control {
+      border-radius: 30px;
+      padding: 10px 20px;
+      background-color: #d6d6d6;
+      transition: background-color 0.3s ease;
+    }
 
+    .form-control:focus {
+      background-color: #fff;
+      border-color: #343a40;
+      box-shadow: none;
+    }
+
+    .btn-submit {
+      display: block;
+      width: 50%;
+      height: 50px;
+      border-radius: 30px;
+      margin: 30px auto 0;
+      font-size: 1.2rem;
+      background-color: #545454;
+    }
+  </style>
 </head>
 
 <body>
-    <h1 class = "text-center"> SIGN UP </h1>
+  <p>LOGIN</p>
+  <div class="card">
+    <form method="post">
+      <div class="mb-4">
+        <label for="username" class="form-label">USERNAME</label>
+        <input type="text" name="username" class="form-control" id="username" required>
+      </div>
 
-    <div class = "container1 shadow">
-     <form>
-        <div class="text-center mt-3">
-            <p style = "font-size: 30px;">USERNAME</p>
-            <input type="email" class="form-control mx-auto"
-                style = "border-radius: 50px;  background-color:rgb(241, 239, 239); width: 70%;" id="username"
-                aria-describedby="emailHelp">
-        </div>
+      <div class="mb-4">
+        <label for="password" class="form-label">PASSWORD</label>
+        <input type="password" name="password" class="form-control" id="password" required>
+      </div>
 
-        <div class="text-center mt-3">
-            <p style = "font-size: 30px;">PASSWORD</p>
-            <input type="password" class="form-control mx-auto"
-                style = "border-radius: 50px; background-color:rgb(241, 239, 239); width: 70%;" id="password">
-        </div>
-
-        <button type="submit" class="button btn btn-dark">Submit</button>
-        </form>
-    </div>
-
+      <button type="submit" class="btn btn-dark btn-submit">LOGIN</button>
+    </form>
+  </div>
 </body>
 
 </html>
